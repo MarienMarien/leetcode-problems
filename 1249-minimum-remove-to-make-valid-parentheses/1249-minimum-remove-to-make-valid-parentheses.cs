95a9dcf68cd759a5cd9193ed5d0d5e140706ff6d
MarienMarien <m.o.bukhanska@gmail.com>
2022-07-17 13:42:42 -0400

    Time: 187 ms (18.18%), Space: 41.1 MB (62.73%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public string MinRemoveToMakeValid(string s) {
+          if (s == null || s.Length == 0)
+                return s;
+            var parenthesesStack = new Stack<KeyValuePair<int, char>>();
+            var openPar = '(';
+            var closingPar = ')';
+            var i = 0;
+            while (i < s.Length) {
+                if (s[i] == closingPar) {
+                    KeyValuePair<int, char> stackTop;
+                    var isEmpty = !parenthesesStack.TryPeek(out stackTop);
+                    if (isEmpty || stackTop.Value == closingPar) {
+                        s = s.Remove(i, 1);
+                        continue;
+                    }
+                    if (stackTop.Value == openPar) {
+                        parenthesesStack.Pop();
+                    }
+                }
+                if (s[i] == openPar) {
+                    parenthesesStack.Push(new KeyValuePair<int, char>(i, s[i]));
+                }
+                i++;
+            }
+            KeyValuePair<int, char> item;
+            while (parenthesesStack.Count > 0) {
+                item = parenthesesStack.Pop();
+                s = s.Remove(item.Key, 1);
+            }
+
+            return s;
+    }
+}
