2cd4e1c743bc0acc53917de30ae29ae3257bd18d
MarienMarien <m.o.bukhanska@gmail.com>
2022-08-17 00:37:38 +0300

    Time: 230 ms (45.11%), Space: 46.1 MB (7.66%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int[] AsteroidCollision(int[] asteroids) {
+        var list = new LinkedList<int>(asteroids);
+        var currAst = list.First.Next;
+        var prev = list.First;
+        while (currAst != null){
+            if (currAst == prev) {
+                currAst = currAst.Next;
+                continue;
+            }
+            if (prev.Value * currAst.Value > 0 || (prev.Value < 0 && currAst.Value > 0))
+            {
+                prev = prev.Next;
+                currAst = currAst.Next;
+                continue;
+            }
+            var toDel = currAst;
+            if (Math.Abs(prev.Value) > Math.Abs(currAst.Value)) {
+
+                currAst = currAst.Next;// check if I can do it
+                list.Remove(toDel);
+                continue;
+            }
+            if (Math.Abs(prev.Value) == Math.Abs(currAst.Value)) {
+                currAst = currAst.Next;
+                list.Remove(toDel);
+            }
+            toDel = prev;
+            prev = prev.Previous == null ? prev.Next : prev.Previous;
+            list.Remove(toDel);
+
+        }
+        return list.ToArray();
+    }
+}
