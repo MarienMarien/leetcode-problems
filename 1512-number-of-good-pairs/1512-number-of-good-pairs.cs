8e2e8e7abb59cf29066a930b38376e8161386b71
MarienMarien <m.o.bukhanska@gmail.com>
2022-08-30 02:16:42 +0300

    Time: 120 ms (54.98%), Space: 36.5 MB (68.78%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cs
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cs
@@ -2,16 +2,17 @@ public class Solution {
     public int NumIdenticalPairs(int[] nums) {
         var set = new Dictionary<int, int>();
         var count = 0;
-        for (var i = 0; i < nums.Length; i++) {
+        for (var i = 0; i < nums.Length; i++)
+        {
             if (!set.ContainsKey(nums[i]))
                 set.Add(nums[i], 0);
             else
+            {
                 set[nums[i]]++;
+                count += set[nums[i]];
+            }
         }
-        foreach (var it in set) {
-            if (it.Value > 0)
-                count += (it.Value * (it.Value + 1)) / 2;
-        }
+
         return count;
     }
 }
