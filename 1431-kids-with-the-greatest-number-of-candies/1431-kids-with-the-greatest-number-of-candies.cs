adc7516cc40635900cb3c34ed4ec21bba92230bf
MarienMarien <m.o.bukhanska@gmail.com>
2022-08-30 02:49:54 +0300

    Time: 165 ms (86.32%), Space: 44.6 MB (10.10%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+        IList<bool> res = new List<bool>();
+        var len = candies.Length;
+        var withExtra = new int[len];
+        var maxCandies = 0;
+        for (var i = 0; i < candies.Length; i++) {
+            withExtra[i] = candies[i] + extraCandies;
+            maxCandies = Math.Max(maxCandies, candies[i]);
+        }
+
+        for (var i = 0; i < candies.Length; i++)
+        {
+            if (withExtra[i] >= maxCandies)
+            {
+                res.Add(true);
+            }
+            else
+                res.Add(false);
+        }
+
+        return res;
+    }
+}
