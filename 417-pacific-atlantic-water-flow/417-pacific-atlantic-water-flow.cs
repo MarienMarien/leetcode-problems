f3a103e34478542be19814c3e80f40de0d559659
MarienMarien <m.o.bukhanska@gmail.com>
2022-09-02 23:15:51 +0300

    Time: 218 ms (86.08%), Space: 46.1 MB (57.28%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cs
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cs
@@ -1,14 +1,15 @@
 public class Solution {
     public IList<IList<int>> PacificAtlantic(int[][] heights) {
         IList<IList<int>> res = new List<IList<int>>();
+
         var rows = heights.Length;
         var cols = heights[0].Length;
         var pacific = new bool[rows,cols];
         var atlantic = new bool[rows,cols];
 
         for (var row = 0; row < rows; row++) {
-            DfsP(0, row, pacific, heights, 0);
-            DfsA(cols - 1, row, atlantic, heights, 0);
+            DfsP(0, row, pacific, heights, 0, 0, 0);
+            DfsP(cols - 1, row, atlantic, heights, 0, cols - 1, rows - 1);
         }
 
         for (var row = 0; row < rows; row++) {
@@ -22,40 +23,21 @@ public class Solution {
         return res;
     }
 
-    private void DfsP(int col, int row, bool[,] pacific, int[][] heights, int prevHeight)
+    private void DfsP(int col, int row, bool[,] pacific, int[][] heights, int prevHeight, int colLimit, int rowLimit)
     {
         if (col < 0 || row < 0 || col >= heights[0].Length || row >= heights.Length || pacific[row, col] == true)
             return;
-        if (col == 0 || row == 0 || prevHeight <= heights[row][col])
+        if (col == colLimit || row == rowLimit || prevHeight <= heights[row][col])
         {
             pacific[row, col] = true;
             // down
-            DfsP(col, row + 1, pacific, heights, heights[row][col]);
+            DfsP(col, row + 1, pacific, heights, heights[row][col], colLimit, rowLimit);
             // right
-            DfsP(col + 1, row, pacific, heights, heights[row][col]);
+            DfsP(col + 1, row, pacific, heights, heights[row][col], colLimit, rowLimit);
             // top
-            DfsP(col, row - 1, pacific, heights, heights[row][col]);
+            DfsP(col, row - 1, pacific, heights, heights[row][col], colLimit, rowLimit);
             // left
-            DfsP(col - 1, row, pacific, heights, heights[row][col]);
-        }
-
-    }
-
-    private void DfsA(int col, int row, bool[,] atlantic, int[][] heights, int prevHeight)
-    {
-        if (col < 0 || row < 0 || col >= heights[0].Length || row >= heights.Length || atlantic[row, col] == true)
-            return;
-        if (col == heights[0].Length - 1 || row == heights.Length - 1 || prevHeight <= heights[row][col])
-        {
-            atlantic[row, col] = true;
-            // down
-            DfsA(col, row + 1, atlantic, heights, heights[row][col]);
-            // left
-            DfsA(col - 1, row, atlantic, heights, heights[row][col]);
-            // top
-            DfsA(col, row - 1, atlantic, heights, heights[row][col]);
-            // right
-            DfsA(col + 1, row, atlantic, heights, heights[row][col]);
+            DfsP(col - 1, row, pacific, heights, heights[row][col], colLimit, rowLimit);
         }
     }
 }
