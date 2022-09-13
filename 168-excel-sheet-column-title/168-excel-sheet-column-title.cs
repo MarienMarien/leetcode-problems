20715d455d33174680e2b55ad04d87c4306055bb
MarienMarien <m.o.bukhanska@gmail.com>
2022-09-14 02:08:14 +0300

    Time: 123 ms (15.77%), Space: 35.3 MB (64.52%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.cs
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cs
@@ -2,16 +2,24 @@ public class Solution {
     public string ConvertToTitle(int columnNumber) {
         var sb = new StringBuilder();
         var curr = columnNumber;
-        while (curr > 26) {
+        while (curr > 26)
+        {
             curr -= 1;
             var newCurr = curr / 26;
             var letter = Convert.ToChar(curr - (newCurr * 26) + 'A');
-            sb.Insert(0, letter);
+            sb.Append(letter);
             curr = newCurr;
         }
-        sb.Insert(0, Convert.ToChar(curr - 1 + 'A'));
+        sb.Append(Convert.ToChar(curr - 1 + 'A'));
 
-
+        int start = 0, end = sb.Length - 1;
+        while (start < end) {
+            var tmp = sb[start];
+            sb[start] = sb[end];
+            sb[end] = tmp;
+            start++;
+            end--;
+        }
         return sb.ToString();
     }
 }
