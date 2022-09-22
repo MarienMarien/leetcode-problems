45acff08ee327d39126279ba40e4973388fd8d15
MarienMarien <m.o.bukhanska@gmail.com>
2022-09-22 21:21:57 +0300

    Time: 108 ms (93.88%), Space: 45.3 MB (5.23%) - LeetHub

--- 136-single-number/136-single-number.cs
+++ 136-single-number/136-single-number.cs
@@ -1,9 +1,12 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-        var res = 0;
+        var set = new HashSet<int>();
         foreach (var n in nums) {
-            res ^= n;
+            if (set.Contains(n))
+                set.Remove(n); 
+            else
+                set.Add(n);
         }
-        return res;
+        return set.First();
     }
 }
