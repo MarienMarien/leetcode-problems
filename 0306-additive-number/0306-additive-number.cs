907ec88b1c6c03cacf6e4955f1f6306643402246
MarienMarien <m.o.bukhanska@gmail.com>
2022-10-30 20:09:28 +0200

    Time: 109 ms (78.95%), Space: 36.4 MB (65.79%) - LeetHub

--- 0306-additive-number/0306-additive-number.cs
+++ 0306-additive-number/0306-additive-number.cs
@@ -1,32 +1,32 @@
 public class Solution {
     public bool IsAdditiveNumber(string num) {
         if (num.Length < 3)
-                return false;
-            for (var i = 0; i < num.Length - 2; i++) {
-                var n1 = Int64.Parse(num.Substring(0, i + 1));
-                var n2 = Int64.Parse(num.Substring(i + 1, 1));
-                if (IsAdditive(n1, n2, num, i + 2))
-                    return true;
-                if (n1 == 0)
+            return false;
+        for (var i = 0; i < (num.Length - 1) / 2; i++) {
+            var n1 = Int64.Parse(num.Substring(0, i + 1));
+            var n2 = Int64.Parse(num.Substring(i + 1, 1));
+            if (IsAdditive(n1, n2, num, i + 2))
+                return true;
+            if (n1 == 0)
+                break;
+        }
+        return false;
+    }
+
+    private static bool IsAdditive(long n1, long n2, string num, int nextId)
+    {
+        if (nextId == num.Length)
+            return true;
+        for (var i = nextId; i < num.Length; i++) {
+            var sum = n1 + n2;
+            var sumStr = sum.ToString();
+            if (num[i..].StartsWith(sumStr) && IsAdditive(n2, sum, num, i + sumStr.Length))
+                return true;
+            else {
+                if (n2 == 0 || !Int64.TryParse(num[(nextId - 1)..(i + 1)], out n2))
                     break;
             }
-            return false;
         }
-
-        private static bool IsAdditive(long n1, long n2, string num, int nextId)
-        {
-            if (nextId == num.Length)
-                return true;
-            for (var i = nextId; i < num.Length; i++) {
-                var sum = n1 + n2;
-                var sumStr = sum.ToString();
-                if (num[i..].StartsWith(sumStr) && IsAdditive(n2, sum, num, i + sumStr.Length))
-                    return true;
-                else {
-                    if (n2 == 0 || !Int64.TryParse(num[(nextId - 1)..(i + 1)], out n2))
-                        break;
-                }
-            }
-            return false;
+        return false;
     }
 }
