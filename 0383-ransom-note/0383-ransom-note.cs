8ae2204018ddf8e457e70eadd0492302ae474cf4
MarienMarien <m.o.bukhanska@gmail.com>
2022-10-30 03:17:02 +0300

    Time: 153 ms (42.54%), Space: 40.2 MB (61.12%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cs
+++ 0383-ransom-note/0383-ransom-note.cs
@@ -2,19 +2,20 @@ public class Solution {
     public bool CanConstruct(string ransomNote, string magazine) {
         if (ransomNote.Length > magazine.Length)
             return false;
-        var note = ransomNote.ToCharArray();
-        Array.Sort(note);
-        var mgzn = magazine.ToCharArray();
-        Array.Sort(mgzn);
-        var noteId = 0;
-        var mgznId = 0;
-        while (mgznId < magazine.Length) {
-            if (noteId == note.Length)
-                return true;
-            if (note[noteId] == mgzn[mgznId])
-                noteId++;
-            mgznId++;
+        var alphabet = new Dictionary<char, int>();
+        for (var i = 0; i < magazine.Length; i++) {
+            if (!alphabet.ContainsKey(magazine[i]))
+                alphabet.Add(magazine[i], 0);
+            alphabet[magazine[i]]++;
         }
-        return noteId == note.Length;
+
+        foreach (var c in ransomNote) { 
+            if(!alphabet.ContainsKey(c))
+                return false;
+            if (alphabet[c] == 0)
+                return false;
+            alphabet[c]--;
+        }
+        return true;
     }
 }
