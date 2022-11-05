894a8d6473d357b90f107ac7404c9d04c7895441
MarienMarien <m.o.bukhanska@gmail.com>
2022-11-05 19:34:30 +0200

    Time: 165 ms (19.68%), Space: 39.3 MB (21.77%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MinCostClimbingStairs(int[] cost) {
+        if (cost.Length < 1)
+            return 0;
+        var stairCosts = new int[cost.Length + 1];
+        stairCosts[0] = cost[0];
+        stairCosts[1] = cost.Length > 1 ? cost[1] : 0;
+        for (var i = 2; i < stairCosts.Length; i++) {
+            var currCost = i < cost.Length ? cost[i] : stairCosts[i];
+            stairCosts[i] = Math.Min(stairCosts[i - 1] + currCost, stairCosts[i - 2] + currCost);
+            Console.WriteLine($"i: {i}; cost: {stairCosts[i]}");
+        }
+        return stairCosts[stairCosts.Length - 1];
+    }
+}
