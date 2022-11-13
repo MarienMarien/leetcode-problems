e18b512ad25d3d577f03819f795dc3e64f9af526
MarienMarien <m.o.bukhanska@gmail.com>
2022-11-13 03:04:11 +0200

    Time: 184 ms (88.80%), Space: 54.5 MB (9.60%) - LeetHub

---
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int SmallestCommonElement(int[][] mat) {
+        var set = new Dictionary<int, int>();
+        var lastRow = mat.Length - 1;
+        for (var row = 0; row < mat.Length; row++) {
+            for (var col = 0; col < mat[0].Length; col++) {
+                if (col > 0 && mat[row][col] == mat[row][col - 1])
+                    continue;
+                if (set.ContainsKey(mat[row][col]))
+                {
+                    if (set[mat[row][col]] == lastRow - 1)
+                        return mat[row][col];
+                    set[mat[row][col]]++;
+                }
+                else {
+                    set.Add(mat[row][col], 0);
+                }
+            }
+        }
+        return -1;
+    }
+}
