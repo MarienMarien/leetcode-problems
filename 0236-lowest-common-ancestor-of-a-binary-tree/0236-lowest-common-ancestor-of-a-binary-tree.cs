cea3db395a35efd5f25197870377bf596f27af11
MarienMarien <m.o.bukhanska@gmail.com>
2022-11-14 01:42:41 +0200

    Time: 279 ms (5.47%), Space: 44.3 MB (5.18%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cs
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private Stack<TreeNode> _pPath;
+    private Stack<TreeNode> _qPath;
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p == q)
+            return p;
+        _pPath = new Stack<TreeNode>();
+        _qPath = new Stack<TreeNode>();
+        FoundSecondPath(root, p, _pPath);
+        FoundSecondPath(root, q, _qPath);
+        var set = new HashSet<TreeNode>(_pPath);
+        while (_qPath.Count > 0) {
+            var curr = _qPath.Pop();
+            if (set.Contains(curr)) {
+                return curr;
+            }
+        }
+        return root;
+    }
+
+    private bool FoundSecondPath(TreeNode root, TreeNode q, Stack<TreeNode> stack)
+    {
+        if (root == null)
+            return false;
+        stack.Push(root);
+        Console.Write($"{root.val} ");
+        if (root == q)
+            return true;
+        var found = FoundSecondPath(root.left, q, stack);
+        if (!found) {
+            
+            found = FoundSecondPath(root.right, q, stack);
+        }
+        if(!found)
+            stack.Pop();
+        return found;
+    }
+}
