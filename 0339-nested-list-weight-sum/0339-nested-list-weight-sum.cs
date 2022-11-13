d4189884516fd43c3b6aa6b676f7ba278fd8c6da
MarienMarien <m.o.bukhanska@gmail.com>
2022-11-13 02:44:57 +0200

    Time: 69 ms (89.39%), Space: 32.2 MB (50.76%) - LeetHub

---
+++ 0339-nested-list-weight-sum/0339-nested-list-weight-sum.cs
@@ -0,0 +1,45 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * interface NestedInteger {
+ *
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool IsInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     int GetInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void SetInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void Add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     IList<NestedInteger> GetList();
+ * }
+ */
+public class Solution {
+    public int DepthSum(IList<NestedInteger> nestedList) {
+        return GetNested(nestedList, 1, 0);
+    }
+
+    private int GetNested(IList<NestedInteger> nestedList, int koef, int ans) {
+        foreach (var nl in nestedList) {
+            if(nl.IsInteger()){
+                ans += nl.GetInteger() * koef;
+            } else {
+                ans = GetNested(nl.GetList(), koef + 1, ans);                
+            }
+        }
+        return ans;
+    }
+}
