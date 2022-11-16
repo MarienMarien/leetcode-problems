51c24c31363ffed80ed5233decdefe91f2434fc6
MarienMarien <m.o.bukhanska@gmail.com>
2022-11-16 22:59:35 +0200

    Time: 134 ms (79.35%), Space: 43.2 MB (26.09%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cs
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cs
@@ -1,30 +1,19 @@
 public class Solution {
     public int SingleNonDuplicate(int[] nums) {
-        if(nums.Length == 1)
-            return nums[0];
-        if (nums[0] != nums[1])
-            return nums[0];
-        if (nums[nums.Length - 1] != nums[nums.Length - 2])
-            return nums[nums.Length - 1];
-        // ???
         if (nums.Length % 2 == 0)
             return -1;
         var start = 0;
         var end = nums.Length - 1;
-        while (start < end) {
+        while (start < end)
+        {
             var mid = start + ((end - start) / 2);
-            if ((end - start) > 1 && mid % 2 == 0)
+            if (mid % 2 == 1)
                 mid--;
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
-                return nums[mid];
             if (nums[mid] == nums[mid + 1])
-            {
+                start += 2;
+            else
                 end = mid;
-            }
-            else {
-                start = mid + 1;
-            }
         }
-        return -1;
+        return nums[start];
     }
 }
