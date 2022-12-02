c310c04449b743f722f1a7651ea772b68f44b07d
MarienMarien <m.o.bukhanska@gmail.com>
2022-12-02 22:02:17 +0200

    Time: 135 ms (84.93%), Space: 50.9 MB (5.82%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cs
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cs
@@ -12,22 +12,23 @@
  * }
  */
 public class Solution {
-    private HashSet<int> _set;
+    private HashSet<int> _complements;// 
     public bool FindTarget(TreeNode root, int k) {
-        _set = new HashSet<int>();
+        _complements = new HashSet<int>();
         return DFS(root, k);
     }
 
-    private bool DFS(TreeNode root, int k)
-    {
-        if (root == null)
+    private bool DFS(TreeNode node, int k){
+        if(node == null)
             return false;
-        if(_set.Contains(root.val))
+        if(_complements.Contains(node.val))
             return true;
-        _set.Add(k - root.val);
-        var res = DFS(root.left, k);
-        if (!res)
-            res = DFS(root.right, k);
-        return res;
-}
+        else{
+            _complements.Add(k - node.val);
+        }
+        //var found = DFS(node.left, k);
+        //if(!found)
+        //    found = DFS(node.right, k);
+        return  DFS(node.left, k) || DFS(node.right, k);
+    }
 }
