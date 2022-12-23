9ffcdf0580ee0eca4f55b1df89485be83e644a46
MarienMarien <m.o.bukhanska@gmail.com>
2022-12-23 03:48:20 +0200

    Time: 598 ms (7.41%), Space: 56.1 MB (25.93%) - LeetHub

---
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.cs
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+    
+    public Node() {
+        val = 0;
+        children = new List<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        children = new List<Node>();
+    }
+    
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+public class Solution {
+    public Node FindRoot(List<Node> tree) {
+        var rootVal = 0;
+        foreach(var node in tree){
+            rootVal += node.val;
+            foreach(var ch in node.children){
+                rootVal -= ch.val;
+            }
+        }
+        Node root = null;
+        foreach(var node in tree){
+            if(node.val == rootVal){
+                root = node;
+                break;
+            }
+        }
+        return root;
+    }
+}
