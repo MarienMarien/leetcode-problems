ebdc9b070b8fe43354052b31d393cddcf8c0fd69
MarienMarien <m.o.bukhanska@gmail.com>
2023-02-07 02:33:07 +0200

    Time: 209 ms (92.65%), Space: 52 MB (75.74%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cs
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cs
@@ -1,31 +1,28 @@
 public class Solution {
     public int TotalFruit(int[] fruits) {
-        Console.WriteLine($"TestStart");
-        var fruit1 = -1;
+        var maxFruits = 0;
+        if(fruits.Length == 0)
+            return maxFruits;
+        var fruit1 = fruits[0];
         var fruit2 = -1;
-        var nextStart = -1;
         var start = 0;
-        var end = 0;
-        var max = 0;
-        while (end < fruits.Length) {
-            if(fruit1 != -1 && fruit2 != -1 && fruits[end] != fruit1 && fruits[end] != fruit2){
-                max = Math.Max(max, end - start);
+        var nextStart = 0;
+        for (var i = 1; i < fruits.Length; i++) {
+            if (fruits[i] != fruit1 && fruit2 < 0) { 
+                fruit2 = fruits[i];
+            }
+            else if (fruits[i] != fruit1 && fruits[i] != fruit2) {
+                maxFruits = Math.Max(maxFruits, i - start);
                 start = nextStart;
-                fruit1 = fruits[nextStart];
-                fruit2 = fruits[end];
-                nextStart = end;
+                nextStart = i;
+                fruit1 = fruits[start];
+                fruit2 = fruits[i];
+                continue;
             }
-            if(fruit1 == -1){
-                fruit1 = fruits[end];
-            }
-            else if(fruit2 == -1 && fruits[end] != fruit1){
-                fruit2 = fruits[end];
-            }
-            if(nextStart == -1 || fruits[end] != fruits[nextStart])
-                nextStart = end;
-            end++;
-       }
-       max = Math.Max(max, end - start);
-       return max;
+            if (fruits[i] != fruits[i - 1])
+                nextStart = i;
+        }
+        maxFruits = Math.Max(maxFruits, fruits.Length - start);
+        return maxFruits;
     }
 }
