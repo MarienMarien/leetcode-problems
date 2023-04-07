ee723f8ccfa11bbd6ce29a8b0dd905763cf245f1
MarienMarien <m.o.bukhanska@gmail.com>
2023-04-07 04:08:15 +0300

    Time: 169 ms (60.27%), Space: 61.1 MB (24.66%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cs
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cs
@@ -1,29 +1,36 @@
 public class Solution {
     public int NumEnclaves(int[][] grid)
     {
-        if(grid.Length < 3 || grid[0].Length < 3)
+        if (grid.Length < 3 || grid[0].Length < 3)
             return 0;
         var onesCount = 0;
-        for (var row = 0; row < grid.Length; row += grid.Length - 1) {
-            for (var col = 0; col < grid[0].Length; col++){
-                if (grid[row][col] == 1) {
-                    _ = Dfs(grid, row, col);
-                }
-            }
-        }
-        for (var col = 0; col < grid[0].Length; col += grid[0].Length - 1)
+        var rows = grid.Length;
+        var cols = grid[0].Length;
+        for (var col = 0; col < cols; col++)
         {
-            for (var row = 0; row < grid.Length; row++)
+            if (grid[0][col] == 1)
             {
-                if (grid[row][col] == 1)
-                {
-                    _= Dfs(grid, row, col);
-                }
+                _ = Dfs(grid, 0, col);
+            }
+            if (grid[rows - 1][col] == 1)
+            {
+                _ = Dfs(grid, rows - 1, col);
             }
         }
-        for (var row = 1; row < grid.Length - 1; row++)
+        for (var row = 0; row < rows; row++)
         {
-            for (var col = 1; col < grid[0].Length - 1; col++)
+            if (grid[row][0] == 1)
+            {
+                _ = Dfs(grid, row, 0);
+            }
+            if (grid[row][cols - 1] == 1)
+            {
+                _ = Dfs(grid, row, cols - 1);
+            }
+        }
+        for (var row = 1; row < rows - 1; row++)
+        {
+            for (var col = 1; col < cols - 1; col++)
             {
                 if (grid[row][col] == 1)
                 {
