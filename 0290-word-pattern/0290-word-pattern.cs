dd16b338076593988b477aff6c5b11d276ebbcb6
MarienMarien <m.o.bukhanska@gmail.com>
2023-04-18 18:50:27 +0300

    Time: 69 ms (94.44%), Space: 37.8 MB (69.75%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cs
+++ 0290-word-pattern/0290-word-pattern.cs
@@ -1,6 +1,31 @@
 public class Solution {
     public bool WordPattern(string pattern, string s) {
         var words = s.Split(" ");
+        if (words.Length != pattern.Length)
+            return false;
+        var matchedSet = new HashSet<string>();
+        var map = new Dictionary<char, string>();
+        for (var i = 0; i < pattern.Length; i++) {
+            if (!map.ContainsKey(pattern[i])) {
+                if (matchedSet.Contains(words[i]))
+                {
+                    map.Add(pattern[i], "");
+                }
+                else
+                {
+                    map.Add(pattern[i], words[i]);
+                    matchedSet.Add(words[i]);
+                }
+            }
+        }
+        for (var i = 0; i < pattern.Length; i++) {
+            if (words[i] != map[pattern[i]])
+                return false;
+        }
+        return true;
+       /*
+       // 2 hashMaps approach
+       var words = s.Split(" ");
         if (pattern.Length != words.Length)
             return false;
         var pToS = new Dictionary<char, string>();
@@ -25,5 +50,6 @@ public class Solution {
             }
         }
         return true;
+        */
     }
 }
