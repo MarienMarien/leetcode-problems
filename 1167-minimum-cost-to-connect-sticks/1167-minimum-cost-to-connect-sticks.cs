34235b0126048d26a02e3cca1dd795f4f278112d
MarienMarien <m.o.bukhanska@gmail.com>
2024-01-23 01:29:18 +0200

    Time: 286 ms (50%), Space: 52.3 MB (9.68%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cs
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cs
@@ -1,22 +1,18 @@
 public class Solution {
     public int ConnectSticks(int[] sticks) {
-        if (sticks.Length == 1)
-            return 0;
         var pq = new PriorityQueue<int, int>();
-        foreach (var s in sticks) {
+        foreach (var s in sticks)
             pq.Enqueue(s, s);
-        }
+
         var totalCost = 0;
-        while (pq.Count > 0) {
-            var a = pq.Dequeue();
-            var isNotLast = pq.TryDequeue(out int b, out _);
-            var sum = a + b;
-            if(isNotLast){
-                pq.Enqueue(sum, sum);
-                totalCost += sum;
-            }
+        while (pq.Count > 1) {
+            var first = pq.Dequeue();
+            var second = pq.Dequeue();
+            var sum = first + second;
+            totalCost += sum;
+            pq.Enqueue(sum, sum);
         }
-        
+
         return totalCost;
     }
 }
