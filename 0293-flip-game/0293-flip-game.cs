8409f99a262cc0062f55a71d730ea6e6053fde56
MarienMarien <m.o.bukhanska@gmail.com>
2024-02-04 00:57:27 +0200

    Time: 101 ms (56.86%), Space: 46.9 MB (68.63%) - LeetHub

--- 0293-flip-game/0293-flip-game.cs
+++ 0293-flip-game/0293-flip-game.cs
@@ -1,16 +1,20 @@
 public class Solution {
     public IList<string> GeneratePossibleNextMoves(string currentState) {
-        IList<string> res = new List<string>();
-        var arr = currentState.ToCharArray();
-        for (var i = 0; i < currentState.Length - 1; i++) {
-            if (currentState[i] == '+' && currentState[i] == currentState[i + 1]) {
-                arr[i] = '-';
-                arr[i + 1] = '-';
-                res.Add(new string(arr));
-                arr[i] = '+';
-                arr[i + 1] = '+';
+        var ans = new List<string>();
+        var sb = new StringBuilder(currentState);
+        var dict = new Dictionary<char, char> { { '+', '-' }, { '-', '+' } };
+
+        for (var i = 1; i < currentState.Length; i++) {
+            if (sb[i] == '+' && sb[i] == sb[i - 1])
+            {
+                sb[i] = dict[sb[i]];
+                sb[i - 1] = dict[sb[i - 1]];
+                ans.Add(sb.ToString());
+                sb[i] = dict[sb[i]];
+                sb[i - 1] = dict[sb[i - 1]];
             }
         }
-        return res;
+
+        return ans;
     }
 }
