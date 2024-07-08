2542b0e11713532f8cea49ece9c73fa6377246bd
MarienMarien <m.o.bukhanska@gmail.com>
2024-07-08 17:15:02 +0300

    Time: 21 ms (81.54%), Space: 26.9 MB (81.54%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cs
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cs
@@ -1,16 +1,12 @@
 public class Solution {
     public int FindTheWinner(int n, int k) {
-        var list = new List<int>();
-        var i = 1;
-        while (i <= n) { 
-            list.Add(i);
-            i++;
+        var winner = 0;
+
+        for (var i = 1; i <= n; i++) 
+        {
+            winner = (winner + k) % i;
         }
-        int index = 0;
-        while (list.Count > 1) { 
-            index = (index + k - 1) % list.Count;
-            list.RemoveAt(index);
-        }
-        return list[0];
+
+        return winner + 1;
     }
 }
