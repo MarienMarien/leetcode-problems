a7d59a801fba8e9e7c5ce0609f43682649787598
MarienMarien <m.o.bukhanska@gmail.com>
2024-07-23 19:41:50 +0300

    Time: 115 ms (40%), Space: 47.2 MB (90%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cs
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cs
@@ -7,23 +7,13 @@ public class Solution {
                 map[n]++;
         }
 
-        var sorted = map.OrderBy(p => p, 
-            Comparer<KeyValuePair<int, int>>.Create((x, y) =>
+        Array.Sort(nums, Comparer<int>.Create((x, y) =>
             {
-                if (x.Value == y.Value)
-                    return y.Key - x.Key;
-                return x.Value - y.Value;
+                if (map[x] == map[y])
+                    return y - x;
+                return map[x] - map[y];
             }));
-        var res = new int[nums.Length];
-        var resId = 0;
-        foreach (var it in sorted)
-        {
-            for (var i = 0; i < it.Value; i++) {
-                res[resId] = it.Key;
-                resId++;
-            }
-        }
 
-        return res;
+        return nums;
     }
 }
