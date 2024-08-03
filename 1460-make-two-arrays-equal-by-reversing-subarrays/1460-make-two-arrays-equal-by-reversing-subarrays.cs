ae280843856b048c32b2508916d49879f21aeb58
MarienMarien <m.o.bukhanska@gmail.com>
2024-08-03 20:07:26 +0300

    Time: 86 ms (77.59%), Space: 45.7 MB (34.48%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cs
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cs
@@ -1,11 +1,17 @@
 public class Solution {
     public bool CanBeEqual(int[] target, int[] arr) {
-        Array.Sort(target);
-        Array.Sort(arr);
-
+        var dict = new Dictionary<int, int>();
         for (var i = 0; i < arr.Length; i++)
         {
-            if (arr[i] != target[i])
+            if (!dict.TryAdd(arr[i], 1))
+                dict[arr[i]] += 1;
+            if (!dict.TryAdd(target[i], -1))
+                dict[target[i]] -= 1;
+        }
+
+        foreach (var n in dict)
+        {
+            if (n.Value != 0)
                 return false;
         }
 
