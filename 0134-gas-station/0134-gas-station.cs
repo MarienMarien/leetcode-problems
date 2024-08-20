60a5c8c08cdf6f9fe9759f7949cd939d050efe4d
MarienMarien <m.o.bukhanska@gmail.com>
2024-08-20 17:14:30 +0300

    Time: 368 ms (93.04%), Space: 72.3 MB (15.03%) - LeetHub

--- 0134-gas-station/0134-gas-station.cs
+++ 0134-gas-station/0134-gas-station.cs
@@ -1,19 +1,21 @@
 public class Solution {
     public int CanCompleteCircuit(int[] gas, int[] cost) {
-        
-        var startPoint = 0;
         var totalTank = 0;
-        var tank = 0;
-        for (var i = 0; i < gas.Length; i++) 
+        var currTank = 0;
+        var start = 0;
+
+        for (var i = 0; i < gas.Length; i++)
         {
-            totalTank += gas[i] - cost[i];
-            tank += gas[i] - cost[i];
-            if (tank < 0)
+            var travel = gas[i] - cost[i];
+            totalTank += travel;
+            currTank += travel;
+            if (currTank < 0)
             {
-                startPoint = i + 1;
-                tank = 0;
+                start = i + 1;
+                currTank = 0;
             }
         }
-        return totalTank >= 0 ? startPoint : -1;
+
+        return totalTank < 0 ? -1 : start;
     }
 }
