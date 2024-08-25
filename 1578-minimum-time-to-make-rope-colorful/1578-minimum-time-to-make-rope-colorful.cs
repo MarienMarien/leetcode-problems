120dd0065c24d950f9c7c46c123c7959e6be69c0
MarienMarien <m.o.bukhanska@gmail.com>
2024-08-25 15:46:35 +0300

    Time: 365 ms (10.53%), Space: 67.7 MB (55.26%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cs
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cs
@@ -1,21 +1,25 @@
 public class Solution {
     public int MinCost(string colors, int[] neededTime) {
-        var sum = neededTime[0];
-        var max = neededTime[0];
-        var totalTime = 0;
-        for (var i = 1; i < colors.Length; i++) {
-            if (colors[i] == colors[i - 1])
+        var minCost = 0;
+        var i = 0;
+
+        while (i < colors.Length)
+        {
+            if (i > 0 && colors[i] == colors[i - 1])
             {
-                sum += neededTime[i];
-                max = Math.Max(max, neededTime[i]);
+                var totalTimeToRemColor = neededTime[i - 1];
+                var longestToRem = neededTime[i - 1];
+                while (i < colors.Length && colors[i] == colors[i - 1])
+                {
+                    totalTimeToRemColor += neededTime[i];
+                    longestToRem = Math.Max(longestToRem, neededTime[i]);
+                    i++;
+                }
+                minCost += totalTimeToRemColor - longestToRem;
             }
-            else {
-                totalTime += sum - max;
-                max = neededTime[i];
-                sum = neededTime[i];
-            }
+            i++;
         }
-        totalTime += sum - max;
-        return totalTime;
+
+        return minCost;
     }
 }
