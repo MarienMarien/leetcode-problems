cca053f5b78de6830b74c9f7138d87aa2f210a7f
MarienMarien <m.o.bukhanska@gmail.com>
2024-09-05 16:58:02 +0300

    Time: 395 ms (32.14%), Space: 91.7 MB (21.43%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.cs
+++ 2028-find-missing-observations/2028-find-missing-observations.cs
@@ -1,21 +1,22 @@
 public class Solution {
     public int[] MissingRolls(int[] rolls, int mean, int n) {
-        var totalSum = (rolls.Length + n) * mean;
-        var initialSum = 0;
-        foreach (var r in rolls)
-            initialSum += r;
-        var neededSum = totalSum - initialSum;
-        if (neededSum < n || neededSum > 6 * n )
+        var totalObservations = rolls.Length + n;
+        var rollsSum = rolls.Sum();
+
+        var missingSum = mean * totalObservations - rollsSum;
+        var possibleMissingVal = missingSum / n;
+        var extra = missingSum % n;
+        if(missingSum < n || missingSum > 6 * n)
             return Array.Empty<int>();
-        var result = new int[n];
-        var missedElement = neededSum / n;
-        var extra = n - (neededSum % n);
-        for (var i = 0; i < n; i++) {
-            if (extra < n && i >= extra) {
-                result[i] = missedElement + 1;
-            } else
-                result[i] = missedElement;
+
+        var missing = new int[n];
+        for (var i = 0; i < n; i++)
+        {
+            missing[i] = possibleMissingVal;
+            if(i < extra)
+                missing[i] += 1;
         }
-        return result;
+
+        return missing;
     }
 }
