b51d9b8a76d34e4c033c1b46dc16e769a2d35534
MarienMarien <m.o.bukhanska@gmail.com>
2024-11-08 13:52:25 +0200

    Time: 1 ms (100%), Space: 99.6 MB (100%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cs
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int[] GetMaximumXor(int[] nums, int maximumBit) {
         var ans = new int[nums.Length];
-        var maxK = (int)Math.Pow(2, maximumBit) - 1;
+        var maxK = (1 << maximumBit) - 1;
         var ansId = nums.Length - 1;
         ans[ansId--] = nums[0] ^ maxK;
 
