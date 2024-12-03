570805b8b67095076941bea23c4ac822558dde4a
MarienMarien <m.o.bukhanska@gmail.com>
2024-12-03 11:17:13 +0200

    Time: 9 ms (85%), Space: 86.3 MB (44%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string-redo.cs
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string-redo.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public string AddSpaces(string s, int[] spaces) {
-        var sb = new StringBuilder();
+        var sb = new StringBuilder(s.Length + spaces.Length);
         var spacesId  =0;
         for(var i = 0; i < s.Length; i++)
         {
