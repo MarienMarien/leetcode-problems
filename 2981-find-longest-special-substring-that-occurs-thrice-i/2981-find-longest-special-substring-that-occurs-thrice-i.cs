c3568126efd6ef0710f48113c99a39c3829de705
MarienMarien <m.o.bukhanska@gmail.com>
2024-12-10 21:54:56 +0200

    Time: 7 ms (27.59%), Space: 41.4 MB (30%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cs
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cs
@@ -1,17 +1,19 @@
 public class Solution {
     public int MaximumLength(string s) {
-        var subStrs = new Dictionary<char, IList<int>>();
+        var subStrs = new Dictionary<char, PriorityQueue<int, int>>();
         var sId = 0;
         while (sId < s.Length)
         {
             var ch = s[sId];
             var count = 1;
             if (!subStrs.ContainsKey(ch))
-                subStrs.Add(ch, new List<int>());
+                subStrs.Add(ch, new PriorityQueue<int, int>());
 
             while (sId < s.Length && s[sId] == ch)
             {
-                subStrs[ch].Add(count);
+                subStrs[ch].Enqueue(count, count);
+                if (subStrs[ch].Count > 3)
+                    subStrs[ch].Dequeue();
                 sId++;
                 count++;
             }
@@ -22,8 +24,8 @@ public class Solution {
         {
             if (sub.Value.Count < 3)
                 continue;
-            var sortedLen = sub.Value.OrderDescending().ToList();
-            longest = Math.Max(longest, sortedLen[2]);
+            var sortedLen = sub.Value.Dequeue();
+            longest = Math.Max(longest, sortedLen);
         }
 
         return longest;
