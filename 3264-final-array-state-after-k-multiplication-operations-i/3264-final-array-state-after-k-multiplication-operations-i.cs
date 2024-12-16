0340214eb01c67908ec5bc27f7d6e2e4154fc884
MarienMarien <m.o.bukhanska@gmail.com>
2024-12-16 12:54:01 +0200

    Time: 5 ms (34.29%), Space: 50.7 MB (14.86%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cs
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cs
@@ -1,19 +1,17 @@
 public class Solution {
     public int[] GetFinalState(int[] nums, int k, int multiplier) {
-        var pq = new PriorityQueue<int, (int, int)>(Comparer<(int, int)>.Create((x, y) => {
-            if (x.Item1 == y.Item1)
-                return x.Item2.CompareTo(y.Item2);
-            return x.Item1.CompareTo(y.Item1);
-        }));
+        var pq = new PriorityQueue<int, (int num, int id)>(Comparer<(int num, int id)>
+            .Create((x, y) => x.num == y.num ? x.id.CompareTo(y.id) : x.num.CompareTo(y.num)));
         for(var i = 0; i < nums.Length; i++)
         {
             pq.Enqueue(i, (nums[i], i));
         }
+
         for(var i = 0; i < k; i++)
         {
-            var currNumsId = pq.Dequeue();
-            nums[currNumsId] *= multiplier;
-            pq.Enqueue(currNumsId, (nums[currNumsId], currNumsId));
+            var currNumId = pq.Dequeue();
+            nums[currNumId] *= multiplier;
+            pq.Enqueue(currNumId, (nums[currNumId], currNumId));
         }
 
         return nums;
