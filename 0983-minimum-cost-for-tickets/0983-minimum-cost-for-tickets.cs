e16418522e21eb67d7bb4e7cf261e255020fa5bc
MarienMarien <m.o.bukhanska@gmail.com>
2024-12-31 12:26:39 +0200

    Time: 0 ms (100%), Space: 41.1 MB (43.75%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cs
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cs
@@ -2,16 +2,23 @@ public class Solution {
     public int MincostTickets(int[] days, int[] costs) {
         var lastDay = days[^1];
         var dp = new int[lastDay + 1];
-        foreach(var d in days)
+        foreach (var d in days)
             dp[d] = 1;
-        for (var i = 1; i <= lastDay; i++) {
-            if (dp[i] == 1)
+
+        for (var day = 1; day <= lastDay; day++)
+        {
+            if (dp[day] != 1)
             {
-                dp[i] = Math.Min(dp[i-1] + costs[0], dp[Math.Max(i-7,0)] + costs[1]);
-                dp[i] = Math.Min(dp[i], dp[Math.Max(i-30, 0)] + costs[2]);
-            } else
-                dp[i] = dp[i - 1];
+                dp[day] = dp[day - 1];
+                continue;
+            }
+
+            dp[day] = Math.Min(
+                Math.Min(dp[day - 1] + costs[0], 
+                    dp[Math.Max(day - 7, 0)] + costs[1]),
+                dp[Math.Max(day - 30, 0)] + costs[2]);
         }
-        return dp[lastDay];
+
+        return dp[^1];
     }
 }
