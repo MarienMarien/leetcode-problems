495401e9bf78185bdda734dae99e8b92d8ac86fb
MarienMarien <m.o.bukhanska@gmail.com>
2025-01-30 22:19:26 +0200

    Time: 3 ms (24.56%), Space: 51.1 MB (15.01%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cs
+++ 0057-insert-interval/0057-insert-interval.cs
@@ -1,32 +1,32 @@
 public class Solution {
     public int[][] Insert(int[][] intervals, int[] newInterval) {
+        var newIntervalStart = newInterval[0];
+        var newIntervalEnd = newInterval[1];
+        Array.Sort(intervals, Comparer<int[]>.Create((x, y) => x[0].CompareTo(y[0])));
+        
         var result = new List<int[]>();
-        var id = 0;
-        var inserted = false;
-        while(id < intervals.Length) {
-            if (!inserted && newInterval[0] <= intervals[id][1]) {
-                if (newInterval[1] < intervals[id][0]) {
-                    result.Add(newInterval);
-                    inserted = true;
-                    continue;
-                }
-                var newEntry = new int[2];
-                newEntry[0] = Math.Min(intervals[id][0], newInterval[0]);
-                newEntry[1] = Math.Max(newInterval[1], intervals[id][1]);
-                id++;
-                while (id < intervals.Length && intervals[id][0] <= newEntry[1]) {
-                    newEntry[1] = Math.Max(newEntry[1], intervals[id][1]);
-                    id++;
-                }
-                result.Add(newEntry);
-                inserted = true;
-                continue;
-            }
-            result.Add(intervals[id]);
-            id++;
+        var i = 0;
+        var intervalsLen = intervals.Length;
+        while(i < intervalsLen && intervals[i][1] < newIntervalStart)
+        {
+            result.Add(intervals[i]);
+            i++;
         }
-        if (!inserted)
-            result.Add(newInterval);
+
+        while(i < intervalsLen && intervals[i][0] <= newIntervalEnd)
+        {
+            newIntervalStart = Math.Min(newIntervalStart, intervals[i][0]);
+            newIntervalEnd = Math.Max(newIntervalEnd, intervals[i][1]);
+            i++;
+        }
+        
+        result.Add(new int[] {newIntervalStart, newIntervalEnd});
+
+        for(; i < intervals.Length; i++)
+        {
+            result.Add(intervals[i]);
+        }
+        
         return result.ToArray();
     }
 }
