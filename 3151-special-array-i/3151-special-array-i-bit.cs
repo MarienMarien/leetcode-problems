3a901e9c72fee61262b098fa03691e182feb1811
MarienMarien <m.o.bukhanska@gmail.com>
2025-02-01 12:42:48 +0200

    Time: 0 ms (100%), Space: 46.2 MB (30%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i-bit.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public bool IsArraySpecial(int[] nums) {
+        for(var i = 1; i < nums.Length; i++)
+        {
+            if((nums[i] & 1) == (nums[i - 1] & 1))
+                return false;
+        }
+        return true;
+    }
+}
