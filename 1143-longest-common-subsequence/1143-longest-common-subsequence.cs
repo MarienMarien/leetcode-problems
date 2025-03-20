b017c6feb054a182f86c06dcc39d0b985d58077f
MarienMarien <m.o.bukhanska@gmail.com>
2025-03-20 16:53:50 +0200

    Time: 21 ms (37.58%), Space: 43.6 MB (62.87%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cs
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cs
@@ -3,16 +3,22 @@ public class Solution {
         var m = text1.Length;
         var n = text2.Length;
         var dp = new int[m + 1,n + 1];
-        for (var row = m - 1; row >= 0; row--) {
-            for (var col = n - 1; col >= 0; col--) {
-                if (text1[row] == text2[col]) {
-                    dp[row, col] = dp[row + 1, col + 1] + 1;
+        for(var i = 1; i <= m; i++)
+        {
+            var t1Id = i - 1;
+            for(var j = 1; j <= n; j++)
+            {
+                var t2Id = j - 1;
+                if(text1[t1Id] == text2[t2Id])
+                {
+                    dp[i,j] = dp[i - 1,j - 1] + 1;
+                    continue;
                 }
-                else {
-                    dp[row, col] = Math.Max(dp[row + 1, col], dp[row, col + 1]);
-                }
+
+                dp[i,j] = Math.Max(dp[i, j - 1], dp[i - 1, j]);
             }
         }
-        return dp[0, 0];
+
+        return dp[m,n];
     }
 }
