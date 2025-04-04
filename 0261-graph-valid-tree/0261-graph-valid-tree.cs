23df4c4d77ab3214d33ba8cda7be1ad1163e5ffb
MarienMarien <m.o.bukhanska@gmail.com>
2025-04-04 20:36:02 +0300

    Time: 0 ms (100%), Space: 47.7 MB (53.06%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cs
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cs
@@ -1,47 +1,64 @@
 public class Solution {
     public bool ValidTree(int n, int[][] edges) {
-        if (edges == null || edges.Length == 0)
-            return n == 1 ? true : false;
-
-        var map = new Dictionary<int, HashSet<int>>();
-        foreach (var e in edges)
+        var uf = new UnionFind(n);
+        foreach(var e in edges)
         {
-            if (!map.TryAdd(e[0], new HashSet<int>() { e[1] }))
-            {
-                map[e[0]].Add(e[1]);
-            }
+            if(!uf.Union(e[0], e[1]))
+                return false;
+        }
 
-            if (!map.TryAdd(e[1], new HashSet<int>() { e[0] }))
+        var rootFound = false;
+        for(var node = 0; node < n; node++)
+        {
+            if(node != uf.Find(node))
+                continue;
+            if(rootFound)
+                return false;
+            rootFound = true;
+        }
+
+        return true;
+    }
+
+    class UnionFind
+    {
+        private int[] _parents;
+        private int[] _ranks;
+        public UnionFind(int n)
+        {
+            _ranks = new int[n];
+            _parents = new int[n];
+            for(var i = 0; i < n; i++)
             {
-                map[e[1]].Add(e[0]);
+                _parents[i] = i;
             }
         }
 
-        if (map.Count != n)
-            return false;
-
-        var q = new Queue<(int node, int parent)>();
-        q.Enqueue((0, -1));
-        var visited = new HashSet<int>() { 0 };
-
-        while (q.Count > 0)
+        public int Find(int x)
         {
-            var curr = q.Dequeue();
-
-            foreach (var e in map[curr.node])
-            {
-                if (visited.Contains(e) && e != curr.parent)
-                {
-                    return false;
-                }
-                if (e != curr.parent)
-                {
-                    q.Enqueue((e, curr.node));
-                    visited.Add(e);
-                }
-            }
+            if(x != _parents[x])
+                _parents[x] = Find(_parents[x]);
+            return _parents[x];
         }
 
-        return visited.Count == n;
+        public bool Union(int a, int b)
+        {
+            var parentA = Find(a);
+            var parentB = Find(b);
+
+            if(parentA == parentB)
+                return false;
+            if(_ranks[parentA] > _ranks[parentB])
+            {
+                _parents[parentB] = parentA;
+                return true;
+            } 
+
+            if(_ranks[parentA] == _ranks[parentB])
+                _ranks[parentA]++;
+            _parents[parentA] = parentB;
+
+            return true;
+        }
     }
 }
