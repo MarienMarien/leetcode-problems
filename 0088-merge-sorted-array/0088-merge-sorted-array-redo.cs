3b095eb0ff139c9e39e7b84a3dde852902bc8dc0
MarienMarien <m.o.bukhanska@gmail.com>
2025-04-08 19:43:26 +0300

    Time: 0 ms (100%), Space: 46.8 MB (72.53%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array-redo.cs
+++ 0088-merge-sorted-array/0088-merge-sorted-array-redo.cs
@@ -2,18 +2,16 @@ public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
         var n1Id = m - 1;
         var n2Id = n - 1;
-
-        for (var i = nums1.Length - 1; i >= 0; i--)
+        for(var i = nums1.Length - 1; i >= 0; i--)
         {
-            if (n2Id < 0 || (n1Id >= 0 && nums1[n1Id] >= nums2[n2Id]))
+            if(n2Id < 0 || (n1Id >= 0 && nums1[n1Id] >= nums2[n2Id]))
             {
                 nums1[i] = nums1[n1Id];
                 n1Id--;
+                continue;
             }
-            else {
-                nums1[i] = nums2[n2Id];
-                n2Id--;
-            }
+            nums1[i] = nums2[n2Id];
+            n2Id--;
         }
     }
 }
