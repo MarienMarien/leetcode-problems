bc37cd929a30e761f5b4ec55db5d3f7df75d7eaa
MarienMarien <m.o.bukhanska@gmail.com>
2025-06-09 11:54:20 +0200

    Time: 1 ms (95.21%), Space: 44 MB (32.92%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks-redo-stack.cs
@@ -0,0 +1,44 @@
+public class MyQueue {
+    private Stack<int> _stack1;
+    private Stack<int> _stack2;
+    private int _top;
+
+    public MyQueue() {
+        _stack1 = new Stack<int>();
+        _stack2 = new Stack<int>();
+    }
+    
+    public void Push(int x) {
+        if(_stack1.Count == 0)
+            _top = x;
+        _stack1.Push(x);
+    }
+    
+    public int Pop() {
+        if(_stack2.Count == 0)
+        {
+            while(_stack1.Count > 0)
+                _stack2.Push(_stack1.Pop());
+        }
+        return _stack2.Pop();
+    }
+    
+    public int Peek() {
+        if(_stack2.Count == 0)
+            return _top;
+        return _stack2.Peek();
+    }
+    
+    public bool Empty() {
+        return _stack1.Count == 0 && _stack2.Count == 0;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Peek();
+ * bool param_4 = obj.Empty();
+ */
