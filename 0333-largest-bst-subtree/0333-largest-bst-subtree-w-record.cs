64d5d82921f84f9b05a5515a624efed15f72dad6
MarienMarien <m.o.bukhanska@gmail.com>
2025-07-09 12:37:54 +0200

    Time: 1 ms (63.64%), Space: 45 MB (25.45%) - LeetHub

---
+++ 0333-largest-bst-subtree/0333-largest-bst-subtree-w-record.cs
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private record NodeStats(bool isBst, int nodesCount, int? maxVal, int? minVal);
+    private int _maxSubTree;
+    public int LargestBSTSubtree(TreeNode root) {
+        _maxSubTree = root == null ? 0 : 1;
+        _ = GetLargest(root);
+        return _maxSubTree;
+    }
+
+    private NodeStats GetLargest(TreeNode node)
+    {
+        if(node == null)
+        {
+            return new NodeStats(true, 0, null, null);
+        }
+
+        var left = GetLargest(node.left);
+        var right = GetLargest(node.right);
+        var isBst = left.isBst && right.isBst;
+        if((left.maxVal != null && node.val <= left.maxVal) || (right.minVal != null && right.minVal <= node.val))
+            isBst = false;
+        var nodesCount = left.nodesCount + right.nodesCount + 1;
+        if(isBst)
+            _maxSubTree = Math.Max(_maxSubTree, nodesCount);
+        return new NodeStats(isBst, nodesCount, right.maxVal??node.val, left.minVal??node.val);
+    }
+}
