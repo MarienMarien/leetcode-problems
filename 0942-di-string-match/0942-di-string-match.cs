44f8a4a34b6d32002fd13f181ac46691a44702c0
MarienMarien <m.o.bukhanska@gmail.com>
2025-07-28 19:31:54 +0200

    Time: 1 ms (73.53%), Space: 52 MB (50%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.cs
+++ 0942-di-string-match/0942-di-string-match.cs
@@ -1,17 +1,51 @@
 public class Solution {
     public int[] DiStringMatch(string s) {
-        var perm = new int[s.Length + 1];
-        var max = s.Length;
-        var min = 0;
-        var id = 0;
-        foreach (var ch in s) { 
-            if(ch == 'D')
-                perm[id] = max--;
-            else
-                perm[id] = min++;
-            id++;
+        var n = s.Length;
+        var str = new int[n + 1];
+        for(var j = 0; j <= n; j++)
+            str[j] = j;
+        
+        var i = 0;
+        const char D = 'D';
+        while(i < n)
+        {
+            if(s[i] != D)
+            {
+                if(str[i] > str[i+1])
+                    Reverse(str, i, i+1);
+                i++;
+                continue;
+            }
+            var revStart = i;
+            while(i < n && s[i] == D)
+            {
+                i++;
+            }
+            
+            var revEnd = i < n ? i + 1 : i;
+            if(revStart == revEnd)
+            {
+                Reverse(str, i, i+1);
+                continue;
+                
+            }
+            
+            Reverse(str, revStart, revEnd);
         }
-        perm[id] = min;
-        return perm;
+        return str;
+    }
+    
+    private void Reverse(int[] str, int from, int to)
+    {
+        var start = from;
+        var end = to;
+        while(start < end)
+        {
+            var tmp = str[start];
+            str[start] = str[end];
+            str[end] = tmp;
+            start++;
+            end--;
+        }
     }
 }
